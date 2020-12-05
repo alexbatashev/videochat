a91b47253dbe64cb868cd1975263c2e5fd38af73
Alexander Batashev <alexbatashev@outlook.com>
2020-12-05 15:11:29 +0300

    Fix clickhouse (#20)
    * Save progress
    
    * Clickhouse fixes

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 # This script exists, because https://github.com/GoogleContainerTools/skaffold/issues/4641
 
 kubectl --context minikube create -f common
+helm repo add bitnami https://charts.bitnami.com/bitnami
 skaffold build --tag=latest --kube-context minikube
 skaffold run --tag="latest" --kube-context minikube
